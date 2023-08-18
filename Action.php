ec3e00a41efa2be60f2a400c38b2ea3ac36bc8c5
Gabi <HunFantasztik@gmail.com>
2023-08-18 20:37:07 +0200

    made action post request

--- Action.php
+++ Action.php
@@ -7,3 +7,7 @@ $queries= new Queries();
 if(isset($_POST["action"]) && $_POST["action"] == "userList"){
   echo json_encode($queries->userList());
 }
+
+if(isset($_POST["action"]) && $_POST["action"] == "register" && $_POST["allDatasKey"]){
+  $queriesRegister= $queries->registerList($_POST["allDatasKey"]["userName"], $_POST["allDatasKey"]["email"], $_POST["allDatasKey"]["password"]);
+}
