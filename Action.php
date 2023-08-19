cf524ef92f6b089d30a99f7f51c344b5f0625a0c
Gabi <HunFantasztik@gmail.com>
2023-08-19 15:32:50 +0200

    made Action Post request

--- Action.php
+++ Action.php
@@ -11,3 +11,7 @@ if(isset($_POST["action"]) && $_POST["action"] == "userList"){
 if(isset($_POST["action"]) && $_POST["action"] == "register" && $_POST["allDatasKey"]){
  echo ($queriesRegister= $queries->registerList($_POST["allDatasKey"]["userName"], $_POST["allDatasKey"]["email"], $_POST["allDatasKey"]["password"])) ;
 }
+
+if(isset($_POST["action"])&& $_POST["action"] == "deleteUser" && $_POST["userIdKey"]){
+  echo($queriesDeleteUser = $queries->deleteUser($_POST["userIdKey"]["id"]));
+}
