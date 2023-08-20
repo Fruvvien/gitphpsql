b7ec9f12dbdaa40ba54a5f050ec73493ed4ef2e0
Gabi <HunFantasztik@gmail.com>
2023-08-20 13:52:14 +0200

    remove navbar from index and did a separately navbar.php

--- index.php
+++ index.php
@@ -9,59 +9,52 @@
     <title>Gitesphpsql</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Gitesphpsql</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="?page=userList">Felhasználók listázása</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="?page=registration">Regisztráció</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="?page=deleteUser">Felhasználó törlése</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="?page=updateUser">Felhasználó szerkesztés</a>
-            </li>
-        </ul>
-        </div>
-    </div>
-    </nav>
+<div class="row" style="padding: 20%" id="loginPanel" >
+<form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary" >Bejelentkezés</button>
+</form>
+</div>
+<script src="login.js"></script>
 
 
-    <div class="container-fluid">
-        <?php
-            if(isset($_GET["page"])){
-                switch($_GET["page"]){
-                    case "userList":
-                        include "userList.php";
-                        break;
-                    case "registration":
-                        include "register.php";
-                        break;
-                    case "deleteUser":
-                        include "delete.php";
-                        break;    
-                    case "updateUser":
-                        include "update.php";
-                        break;    
-                    default:
-                        include "userList.php";
-                        break;
-                }
-                    
-            }else{
-                include "userList.php";
+<div class="container-fluid">
+    <?php
+        if(isset($_GET["page"])){
+            switch($_GET["page"]){
+                case "userList":
+                    include "userList.php";
+                    break;
+                case "registration":
+                    include "register.php";
+                    break;
+                case "deleteUser":
+                    include "delete.php";
+                    break;    
+                case "updateUser":
+                    include "update.php";
+                    break;    
+                default:
+                    include "userList.php";
+                    break;
             }
+                    
+        }
 
-        ?>
-    </div>
+    ?>
+</div>
         
 </body>
 </html>
---
+++ navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Gitesphpsql</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="?page=userList">Felhasználók listázása</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="?page=registration">Regisztráció</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="?page=deleteUser">Felhasználó törlése</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="?page=updateUser">Felhasználó szerkesztés</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+    </nav>
